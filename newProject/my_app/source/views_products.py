6a7d03dbeb46a6835ce97c9896dfa66725e95e7a
Kai Stullich <kaistullich@rocketmail.com>
2016-11-24 22:35:37 -0800

    added search parameters

--- newProject/my_app/source/views_products.py
+++ newProject/my_app/source/views_products.py
@@ -27,4 +27,64 @@ def product(key):
 
     return render_template('product.html', single_product=item)
 
-# --------------------------
+# ------------------ PRODUCT SEARCHING ---------------------------
+
+def product_search():  
+    name = request.args.get('name')
+    price = request.args.get('price')
+    price_greater_equal = request.args.get('price_ge')
+    price_smaller_equal = request.args.get('price_se')
+    category = request.args.get('category')
+    brand = request.args.get('brand')
+    year = request.args.get('year')
+    rating = request.args.get('rating')
+    stock = request.args.get('stock')
+    
+    condition = ""
+    if name != None:
+        condition += "product.name LIKE '%" +name+ "%'"
+    if price != None:
+        if condition !="":
+            condition += " AND "
+        condition += "product.price=" +str(price)
+    if category != None:
+        if condition != "":
+            condition += " AND "
+        condition  += "category.name LIKE '%" +category+ "%'"
+    if brand != None:
+        if condition != "":
+            condition += " AND "
+        condition  += "product.brand LIKE '%" +brand+ "%'"
+    if price_greater_equal != None:
+        if condition != "":
+            condition += " AND "
+        condition  += "product.price >= " + str(price_greater_equal)
+    if price_smaller_equal != None:
+        if condition != "":
+            condition += " AND "
+        condition  += "product.price <= " + str(price_smaller_equal) 
+            
+           
+    if condition == "":
+        command = """SELECT {a}.id, {a}.name, {a}.price, {b}.name
+                          FROM {a} 
+                          JOIN {b} 
+                          ON {a}.category_id = {b}.id
+            """.format(a="product", b='category')        
+    else:
+        command = """SELECT {a}.id, {a}.name, {a}.price, {b}.name
+                          FROM {a} 
+                          JOIN {b} 
+                          ON {a}.category_id = {b}.id
+                          WHERE {cond}
+            """.format(a="product", b='category', cond = condition)
+       
+    cursor.execute(command)
+    product_data = cursor.fetchall()
+    return render_template('products.html', my_list=product_data)
+
+
+
+
+
+    # --------------------------
