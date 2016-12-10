a4743ce651b2ca09dfe64aca446ee4b3abdc4c88
Kai Stullich <kaistullich@rocketmail.com>
2016-12-09 18:44:36 -0800

    removed all editing code from views category & views product

--- newProject/my_app/source/views_categories.py
+++ newProject/my_app/source/views_categories.py
@@ -1,5 +1,4 @@
 from flask import render_template, request, redirect, url_for, flash
-from my_app.source.models import CategoryForm
 from my_app.source.models import cursor, conn
 
 #-------------------- Category Handler --------------------
@@ -11,6 +10,9 @@ def categories():
     category_data = cursor.fetchall()  
     
     return render_template('categories.html', my_list=category_data)
+
+
+
 
 #-------------------- Category Key Handler --------------------
 def category(key):
@@ -31,105 +33,7 @@ def category(key):
     return render_template('category.html', category_id=key, category_name=category_name, 
                             my_list=product_data)
 
-# ----------------- Category Create -----------------
-def category_create():
-    command = """ SELECT MAX(id)
-                    FROM category
-            """
-    cursor.execute(command)
-    next_id = cursor.fetchone() 
-    category_id = next_id[0]+1
-    
-    form = CategoryForm(request.form)
-       
-    result = None
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        deptPhone = form.deptPhone.data
-        deptLine = form.deptLine.data
-        deptManager = form.deptManager.data
-        
-        command = """
-            INSERT INTO category 
-            (id,name, deptPhone, deptLine, deptMang) VALUES 
-            ({i},'{n}', {dp}, {dl}, '{dm}')
-            """.format(i=category_id, n=name, dp=deptPhone, dl=deptLine, dm=deptManager)
-        
-        cursor.execute(command)
-        conn.commit()
-        # if form is validated it will flash this message
-        flash('The category "%s" has been created successfully!' % (name))
-        return redirect(url_for('my_view.categories'))
-    # if there is an error with the form it will flash the message
-    if form.errors:
-        flash(form.errors)
-            
-    return render_template('category-create.html', form=form, result=result)
 
-# -------------------------- Edit Category ----------------------------------------
-def category_edit(key):
-    # fetches all the names of the categories
-    command = """ SELECT *
-                  FROM category 
-                  WHERE id = {k}""".format(k=key)
-    cursor.execute(command)
-    all_categories = cursor.fetchall()
-    # Pre-populating the form when editing categories
-    if all_categories is None:
-        form = CategoryForm(request.form)
-    else:
-        single_category = all_categories[0]
-        form = CategoryForm(request.form, \
-                            name=single_category[1], \
-                            deptPhone=single_category[2], \
-                            deptLine=single_category[3], \
-                            deptManager=single_category[4])
-
-    # checks if the request method is POST and if form is validated
-    if request.method == 'POST':
-        name = request.form['name']
-        deptPhone = request.form['deptPhone']
-        deptLine = request.form['deptLine']
-        deptManager = request.form['deptManager']
-
-        command = """
-            UPDATE category SET name = '{n}', deptPhone={dp}, deptLine={dl}, deptMang='{dm}'
-            WHERE  id = {i}
-            """.format(n=name, dp=deptPhone, dl=deptLine, dm=deptManager, i=key)
-        cursor.execute(command)
-        conn.commit()
-        # If succesfull it will falsh the message to the user
-        flash('The category "%s" has been edited successfully!' % (name))
-        return redirect(url_for('my_view.categories'))
-    # if there is an error with the form it will flash the message
-    if form.errors:
-        flash(form.errors)
-
-    return render_template('category-edit.html', form=form, category_id=key, all_categories=all_categories)
-
-# ----------------------- Category Delete -------------------------------------
-
-def category_delete():
-    #selects all category names, id from product table
-    command = """ SELECT id, name
-                  FROM category """
-    cursor.execute(command)
-    categories = cursor.fetchall()
-    
-    #deletes product selected from product table
-    if request.method == 'POST':
-        #Command to delete goes here
-        category_id = request.form['category']
-        command = """ DELETE FROM category
-                    WHERE category.id = {p1}
-                  """.format(p1=category_id)
-        cursor.execute(command)
-        conn.commit()
-        # if form is validated it will flash this message
-        flash('The category has been deleted successfully!')
-        return redirect(url_for('my_view.categories'))
-
-    return render_template('category-delete.html', categories=categories )
 
 # ------------------ Contact Us Phone Numbers -------------------
 def contact_us():
--- newProject/my_app/source/views_products.py
+++ newProject/my_app/source/views_products.py
@@ -1,7 +1,4 @@
-from __future__ import print_function
-import sys
 from flask import render_template, request, redirect, url_for, flash
-from my_app.source.models import ProductForm
 from my_app.source.models import cursor, conn
 
 #------------------------ SHOW ALL products-------------------
@@ -16,6 +13,8 @@ def products():
 	product_data = cursor.fetchall()  
 	# takes 'command' and renders the template products.html
 	return render_template('products.html', my_list=product_data)
+
+
 
 # ------------------------SINGLE product ---------------------
 def product(key):    
@@ -37,130 +36,9 @@ def product(key):
     return render_template('product.html', single_product=item, key=key)
 
 
-# ---------------------- CREATE a new product -------------------
-def product_create():
-    # this will fetch the max ID from product table, to create a new product
-    command = """ SELECT MAX(id)
-                    FROM product """
-    cursor.execute(command)
-    next_id = cursor.fetchone()   
-    product_id = next_id[0]+1
-    # re-assings the ProductForm class from models.py into variable  
-    form = ProductForm(request.form)
-    #Selects all from category
-    command = """ SELECT * FROM category """
-    cursor.execute(command)
-    categories = cursor.fetchall()
-    # checks if the request method == POST and if the form is validated
-    if request.method == 'POST' and form.validate():
-        brand = form.brand.data
-        name = form.name.data
-        price = form.price.data
-        rating = form.rating.data
-        year = form.year.data
-        stock = form.stock.data
-        image = form.image.data
-        category = request.form['category']
-        # will insert the new product into the product table
-        command = """
-            INSERT INTO product 
-            (id,brand,name,price,rating,category_id,year,stock,image)
-            VALUES 
-            ({i},'{b}','{n}',{p},{r},{c},{y},{s},'{img}')
-            """.format(i=product_id,b=brand,n=name,p=price,r=rating,c=category,y=year,s=stock,img=image)#place category_id between rating & year when fixed
-        cursor.execute(command)
-        conn.commit()
-        # If succesfull it will falsh the message to the user
-        flash('The product "%s" has been created successfully!' % (name))
-        # when form is submitted it will redirect user to the products page
-        return redirect(url_for('my_view.product', key=product_id))
-    # if there is an error with the form it will flash the message
-    if form.errors:
-        flash(form.errors)
-    # this renders the template
-    return render_template('product-create.html', form=form, product_id=product_id, categories=categories)
-
-# --------------------- EDIT a product ----------------------------
-def product_edit(key):
-    # this will fetch all product names for dropdown menu
-    command = """ SELECT *
-                  FROM product 
-                  WHERE product.id = {k}""".format(k=key)
-    cursor.execute(command)
-    product = cursor.fetchall()
-    # Pre-populating the form when editing products
-    if product is None:
-        form = ProductForm(request.form)
-    else:
-        single_product = product[0]
-        form = ProductForm(request.form, \
-                           brand=single_product[0], \
-                           name=single_product[1], \
-                           price=single_product[2], \
-                           rating=single_product[3], \
-                           year=single_product[5], \
-                           stock=single_product[6], \
-                           image=single_product[7])
-    # this will fetch all categories for dropdown menu
-    command = """ SELECT *
-                  FROM category """
-    cursor.execute(command)
-    categories = cursor.fetchall()
-    # checks if the request method == POST and if the form is validated
-    if request.method == 'POST' and form.validate():
-        brand = form.brand.data
-        name = form.name.data
-        price = form.price.data
-        rating = form.rating.data
-        year = form.year.data
-        stock = form.stock.data
-        image = form.image.data
-        category = request.form['category']
-        # will update the product chosen inside of the product table
-        command = """
-            UPDATE product SET brand='{b}', name='{n}', price={p}, rating={r}, category_id={c}, year={y}, stock={s}, image='{img}'
-            WHERE  id = {i}
-            """.format(b=brand, n=name, p=price, r=rating, c=category, y=year, s=stock, img=image, i=key)
-        cursor.execute(command)                                                              
-        conn.commit()
-        # If succesfull it will falsh the message to the user
-        flash('The product "%s" has been edited successfully!' % (name))
-        # will redirect user to the edited product
-        return redirect(url_for('my_view.product', key=key))
-    # if there is an error with the form it will flash the message
-    if form.errors:
-        flash(form.errors)
-    # this renders the template
-    return render_template('product-edit.html', form=form, categories=categories, product=product, category_id=key)
-
-
-# ----------------------- Product Delete -----------------
-def product_delete():
-    #selects all product names, id from product table
-    command = """ SELECT id, name
-                  FROM product """
-    cursor.execute(command)
-    products = cursor.fetchall()
-    
-    #deletes product selected from product table
-    if request.method == 'POST':
-        # Command to delete goes here
-        product_id = request.form['product']
-        command = """ DELETE FROM product
-                    WHERE product.id = {p1}
-                  """.format(p1=product_id)
-        cursor.execute(command)
-        conn.commit()
-        # If succesfull it will falsh the message to the user
-        flash('The product has been deleted successfully!')
-        # if method = POST it will redirect to the show all products page
-        return redirect(url_for('my_view.products'))   
-    # renders the product delete template
-    return render_template('product-delete.html', products=products )
-
 
 # ------------------ PRODUCT SEARCHING ---------------------------
-# Produc Search Function
+# Product Search Function
 def product_search():  
     # All the arguments 
     name = request.args.get('name')
