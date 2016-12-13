7004e48775c1191b00baa800cb538d634f8df62c
Kai Stullich <kaistullich@rocketmail.com>
2016-12-12 22:35:41 -0800

    add :'s to Registration Class

--- newProject/my_app/source/models.py
+++ newProject/my_app/source/models.py
@@ -7,12 +7,12 @@ conn = sqlite3.connect(sqlite_file)
 cursor = conn.cursor()
 
 class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=20)])
-    email = TextField('Email Address', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
+    username = TextField('Username:', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address:', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password:', [
         validators.Required(),
         validators.Length(min=6, max=15),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Confirm Password')
+    confirm = PasswordField('Confirm Password:')
     accept_tos = BooleanField('I accept the Terms of Service and Privacy', [validators.Required()])
